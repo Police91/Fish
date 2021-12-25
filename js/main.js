6596c7b8b60388540be8483dca10cef6acc6d51b
Andrii Girnyi <andriy.police91@gmail.com>
2021-12-25 23:13:37 +0200

    Section Product-Seafood

--- js/main.js
+++ js/main.js
@@ -10,16 +10,20 @@ $(function(){
     });
 
     $('.product__name').slick({
-        slidesToShow: 6,
+        slidesToShow: 4,
         slidesToScroll: 1,
         focusOnSelect: true,
         asNavFor: '.product__content',
-        vertical: true
+        vertical: true,
+        centerMode: true,
+        prevArrow: '<button type="button" class="product-prev"><img src="./images/product-prev.svg" alt="prev"></button>',
+        nextArrow: '<button type="button" class="product-next"><img src="./images/product-next.svg" alt="next"></button>'
     });
     $('.product__content').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         asNavFor: '.product__name',
-        fade: true
+        fade: true,
+        arrows: false,
     });
 });
